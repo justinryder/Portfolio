7cf3e5e9c72a590e25d470cfff31694e28a17d6c
Justin Ryder <justin@justin-ryder.com>
2016-11-30 05:12:47 +0000

    Updated port and path for digitalocean hosting.

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+var path = require('path');
 var express = require('express');
 var app = express();
 
-var appDirectory = __dirname + '\\app';
+var appDirectory = path.join(__dirname, 'app');
 app.use(express.static(appDirectory));
 console.log('server using static ' + appDirectory);
 
-var port = process.env.PORT || 3000;
+var port = 80;
 app.listen(port);
 console.log('server listening on port ' + port);
